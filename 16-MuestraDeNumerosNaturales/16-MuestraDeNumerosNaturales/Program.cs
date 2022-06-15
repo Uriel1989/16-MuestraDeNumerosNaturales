f913891ccf1cd48cb12c4801aaf042a0fd3c76cc
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:31:13 -0300

    Add files via upload

---
+++ 16-MuestraDeNumerosNaturales/16-MuestraDeNumerosNaturales/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _16_MuestraDeNumerosNaturales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+
+
+            Console.WriteLine("Ingrese un numero entero y positivo: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+
+                Console.WriteLine("Los numeros naturales son: " + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
